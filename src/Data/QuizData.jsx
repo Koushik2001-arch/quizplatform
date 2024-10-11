105aec1272c248da89a62cf0da90fbeb4039efa5
Koushik2001-arch <126883964+Koushik2001-arch@users.noreply.github.com>
2024-10-11 20:20:48 +0530

    Initial commit

---
+++ src/Data/QuizData.jsx
@@ -0,0 +1,72 @@
+export const QuizData = [
+  {
+      question: "Which language runs in a web browser?",
+      options:[ "Java", "C", "Python", "JavaScript"],
+      answer: 4
+  },
+  {
+      question: "What does CSS stand for?",
+      options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+      answer: 2
+  },
+  {
+      question: "What does HTML stand for?",
+      options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+      answer: 1
+  },
+  {
+      question: "What year was JavaScript launched?",
+      options: ["1996", "1995", "1994", "none of the above"],
+      answer:2
+  },
+  {
+    question: "which of the following is the correct name of React.js?",
+    options: ["React","React.js","ReactJS","All of the above"],
+    answer : 4
+  },
+  {
+    question:"Which of the following are the advantages of React.js?",
+    options: ["React.js can increase the application's performance with Virtual DOM.","React.js is easy to integrate with other frameworks such as Angular, BackboneJS since it is only a view library.","React.js can render both on client and server side.","All of the above"],
+    answer: 4
+  },
+  {
+    question: "Which of the following is not a disadvantage of React.js?",
+    options: ["React.js has only a view layer. We have put your code for Ajax requests, events and so on.","The library of React.js is pretty large.","The JSX in React.js makes code easy to read and write.","The learning curve can be steep in React.js."],
+    answer : 3
+  },
+  {
+    question : "Which of the following command is used to install create-react-app?",
+    options : ["npm install -g create-react-app","npm install create-react-app","npm install -f create-react-app","install -g create-react-app"],
+    answer : 1
+  },
+  {
+    question : " What of the following is used in React.js to increase performance?",
+    options : ["Original DOM","Virtual DOM","Both A and B.","None of the above."],
+    answer : 2
+  },
+  {
+    question : "A class is a type of function, but instead of using the keyword function to initiate it, which keyword do we use?",
+    options : ["Constructor","Class","Object","DataObject"],
+    answer : 2
+  },
+  {
+    question : "Which of the following acts as the input of a class-based component?",
+    options : ["Class","Factory","Render","Props"],
+    answer : 4
+  },
+  {
+    question : "Which of the following keyword is used to create a class inheritance?",
+    options : ["Create","Inherits","Extends","This"],
+    answer : 3
+  },
+  {
+    question : "What is the default port where webpack-server runs?",
+    options : [3000,8080,3030,6060],
+    answer : 2
+  },
+  {
+    question : " How many numbers of elements a valid react component can return?",
+    options : [1,2,4,5],
+    answer : 1
+  }
+];
